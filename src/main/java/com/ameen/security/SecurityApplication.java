9f77fd2c671f2425bfdba981e0d13472b344b968
AmeenNH3 <md.ameenullah.h@gmail.com>
2022-06-05 20:03:27 +0530

    commit

--- src/main/java/com/ameen/security/SecurityApplication.java
+++ src/main/java/com/ameen/security/SecurityApplication.java
@@ -1,11 +1,14 @@
 package com.ameen.security;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
 @EnableWebSecurity
 @EnableMongoAuditing
 public class SecurityApplication {
